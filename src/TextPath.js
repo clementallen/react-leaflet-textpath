43c024284b5346f599134d69942655f86239fc02
Clement Allen <clementallen313@gmail.com>
2019-01-14 10:42:28 +0000

    Added tests for TextPath component

--- src/TextPath.js
+++ src/TextPath.js
@@ -2,45 +2,47 @@ import { Path } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet-textpath';
 
-export default class TextPath extends Path {
-    createLeafletElement(props) {
-        // props.leaflet is not used but destructured out so it's not passed to L.Polyline
-        const {
-            positions,
+// props.leaflet is not used but destructured out so it's not passed to L.Polyline
+function getOptions({
+    positions,
+    text,
+    repeat,
+    center,
+    below,
+    offset,
+    orientation,
+    attributes,
+    leaflet, // eslint-disable-line no-unused-vars
+    ...options
+}) {
+    return {
+        polyline: { positions, options },
+        textpath: {
             text,
             repeat,
             center,
             below,
             offset,
             orientation,
-            attributes,
-            leaflet, // eslint-disable-line no-unused-vars
-            ...options
-        } = props;
-        const line = new L.Polyline(positions, options);
-        line.setText(text, {
-            repeat,
-            center,
-            below,
-            offset,
-            orientation,
             attributes
-        });
+        }
+    };
+}
+
+export default class TextPath extends Path {
+    createLeafletElement(props) {
+        const {
+            polyline: { positions, options },
+            textpath: { text, ...pathOptions }
+        } = getOptions(props);
+        const line = new L.Polyline(positions, options);
+        line.setText(text, pathOptions);
         return line;
     }
 
-    updateLeafletElement(
-        fromProps,
-        { text, repeat, center, below, offset, orientation, attributes }
-    ) {
+    updateLeafletElement(fromProps, toProps) {
+        const { text, ...options } = getOptions(toProps).textpath;
         this.leafletElement.setText(null);
-        this.leafletElement.setText(text, {
-            repeat,
-            center,
-            below,
-            offset,
-            orientation,
-            attributes
-        });
+        this.leafletElement.setText(text, options);
     }
 }
